40cd84878681900109f332f48c9ae65917042657
YachaTree <dnftksghkfkd@icloud.com>
2025-03-09 00:43:10 +0900

    Fix: ESLint 설정 개선

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,7 +1,18 @@
 module.exports = {
-  // 기존 설정 유지
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended'
+  ],
   parserOptions: {
     parser: '@babel/eslint-parser',
     requireConfigFile: false
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 } 
